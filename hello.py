4f92c396767c39865e7a2c95d2358cbc34ffb62a
qazz92 <j.rokhun@gmail.com>
2017-11-03 07:05:39 +0900

    fix time5

--- hello.py
+++ hello.py
@@ -1074,7 +1074,7 @@ def send_mail(today_content, today_email):
 
 
 # 오늘 메일을 보내야하는 사람 선택하기
-@scheduler.scheduled_job('cron', day_of_week='mon-sun', hour=6, minute=50)
+@scheduler.scheduled_job('cron', day_of_week='mon-sun', hour=7, minute=7)
 @app.route('/select_mail', methods=['GET'])
 def select_mail():
     conn = mysql.connect()
